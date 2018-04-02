fd88424bb336ff34ea1c148bf4d6a60ad1196a00
kim gi chul <21500048@handong.edu>
2018-04-02 20:47:47 +0900

    first commit

--- src/main/java/practice1/Practice1.java
+++ src/main/java/practice1/Practice1.java
@@ -1,10 +1,36 @@
 package practice1;
+/**
+ * 
+ * 
+ * @author GiCheol
+ *
+ */
 
 public class Practice1 {
 
+	
 	public static void main(String[] args) {
+			int multi=14;
 		// TODO Auto-generated method stub
+			System.out.println("민호야 재밌니 민호가 보는것 + 나이 + 키");
+			System.out.println("multi :" + multi);	
+			MinHoWhattheHell(24,180.00);
+			
+	}	
 
+	
+	
+	
+	
+	
+	
+	public static int MinHoWhattheHell(int age, double height) 
+	{
+	System.out.println("he`s watching utube+he`s age is "+age *height +" 키는 :" + height);	
+	int multi=13;
+	System.out.println("multi :" +multi);	
+	return multi;
 	}
-
+		
 }
+
